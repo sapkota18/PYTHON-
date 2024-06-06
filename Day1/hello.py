7c950c365a4e36a51031195986ec401f143933ba
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-06-06 17:58:02 +0545

    basic of python comments variables etc

---
+++ Day1/hello.py
@@ -0,0 +1,46 @@
+# x=65
+# y="Nepal"
+# print(x)
+# print(y)
+# print(x,y)
+# _my="kashi"
+# print(_my)
+# print(x,y,_my)
+# x,y="nepal","China"
+# print(x,y)
+# z="3"+"3"
+# print(z)
+# c=4+"Avishar"
+# print(c)
+# x="Kashiram"
+# print("my name is",x)
+# x=int(1)
+# y=int(1.9)
+# z=int(1.9999999)
+# b=int(5.9999)
+# print(x,y)
+# print(z)
+# print(b)
+# x=str(1)
+# y="word"
+#list Example
+# print(x+y)
+# x=[1,1,2,3,'word','house']
+# y=x+['cow']
+# print(x)
+# print(y)
+
+#Tuple()
+# x=(1,1,2,3,'house','cow')
+# print(x)
+
+# #set{}
+# x={1,1,2,3,'house','cow'}
+# print(x)
+
+#Dict{}
+x={'a':'apple','b':'ball','a':'ab'}
+print(x)
+
+
+
