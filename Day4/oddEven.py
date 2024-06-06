5bb5ad2aa175806c406ed66ff5b8b07adb5de330
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-06-06 18:22:04 +0545

    Odd Even Checker

--- Day4/oddEven.py
+++ Day4/oddEven.py
@@ -1,12 +1,14 @@
 
-x=float(input(("Enter a Number To check even or odd: ")))
-if type(x)==int:
-    if(x%2==0):
-        print(x,"is even")
+x = float(input("Enter a Number To check even or odd: "))
+if x.is_integer():  # Check if the float is a whole number
+    x = int(x)  # Convert the float to an integer
+    if x % 2 == 0:
+        print(x, "is even")
     else:
-        print(x,"is odd")
+        print(x, "is odd")
 else:
-    print("Input should be int")        
+    print("Input should be an integer")
+   
  
     
     
