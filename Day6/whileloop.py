fe8267dad3fe1ea34ebc46261f2c6b65f32f6e3d
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-06-17 16:43:22 +0545

    While loop and problem related to it solution

---
+++ Day6/whileloop.py
@@ -0,0 +1,58 @@
+# i=1
+# while i<6:
+#     print(i)
+#     if(i==3):
+#         break
+#     i+=1
+
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# n=int(input("upto which number: "))
+# i=1
+# sum=0
+# while i<=n:
+#     sum+=i
+#     i+=1
+# print("Sum of natural number upto",n,"is",sum)
+
+# i=1
+# fact=1
+# n=int(input("upto which number: "))
+# while i<=n:
+#     fact=fact*i
+#     i+=1
+# print("Factorial",fact)
+
+
+# num = int(input("Enter a number: "))
+# reversed_num = 0
+# while num > 0:
+#     digit = num % 10
+#     reversed_num = reversed_num * 10 + digit
+#     num = num //10
+# print("Reversed number:", reversed_num) 
+
+
+# def reversed_number(n):
+#     reversed_num = 0
+#     while n > 0:
+#         digit = n % 10
+#         reversed_num = reversed_num * 10 + digit
+#         n = n // 10
+#     return reversed_num
+
+# num = int(input("Number: "))
+# result = reversed_number(num)
+# print("Reversed Number is:", result)
+
+
