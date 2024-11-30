84762e1baa4c73d1ce0a766ab6c3b65f6e678684
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-11-30 22:25:24 +0545

    finding larfest and smallest element in array

---
+++ OOPS/shorting.py
@@ -0,0 +1,19 @@
+array = [4, 5, 6, 7, 8, 9, 9, 6,0]
+
+def find_smallest_and_largest(array):
+   
+    smallest = array[0]
+    largest = array[0]
+    
+   
+    for num in array:
+        if num < smallest:
+            smallest = num
+        if num > largest:
+            largest = num
+    
+    return smallest, largest
+
+smallest, largest = find_smallest_and_largest(array)
+print("Smallest element:", smallest)
+print("Largest element:", largest)
