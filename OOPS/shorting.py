7ccfc59737d82bc15a0a8911eecbb8f7aec251cf
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-12-01 17:36:04 +0545

    sorting array

--- OOPS/shorting.py
+++ OOPS/shorting.py
@@ -17,3 +17,13 @@ def find_smallest_and_largest(array):
 smallest, largest = find_smallest_and_largest(array)
 print("Smallest element:", smallest)
 print("Largest element:", largest)
+
+array2 = [4, 5, 6, 7, 8, 9, 5, 4]
+def sort_array(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+sorted_array = sort_array(array2)
+print(sorted_array)
