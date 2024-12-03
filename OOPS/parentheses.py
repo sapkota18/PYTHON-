62777c3258d755bdcedb76e31fec5643b4dc4f94
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-12-03 19:37:12 +0545

    Parentheses checker

--- OOPS/parentheses.py
+++ OOPS/parentheses.py
@@ -16,18 +16,10 @@ for string in test_strings:
     print(f"{string}: {'Balanced' if is_balanced(string) else 'Not Balanced'}")
 
 
-def find_pairs_with_sum(nums,target):
-    seen=set()
-    pairs=[]
+
+
+
+
+
     
-    for num in nums:
-        complement = target - num
-        if complement in seen:
-            pairs.append((complement,num))
-        seen.add(num)
-    return pairs
-nums = [1, 2, 3, 4, 5]
-target = 6
-result = find_pairs_with_sum(nums, target)
-print(result)
-         
+    
